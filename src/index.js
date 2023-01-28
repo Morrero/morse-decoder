28f0d63df3e3eee9249a6be8f11ee75ca3e99a3a
Morrero <111439650+Morrero@users.noreply.github.com>
2023-01-28 04:55:09 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -37,10 +37,51 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode1(expr) {
+    let string = "";
+    for (let i = 0; i < expr.length; i+=10){
+        let letter = "";
+        let character = expr.slice(i, i + 10);
+        if (character === "**********"){
+            string = string + " ";
+        }else{
+            for (let j = 0; j < character.length; j+=2){
+                let cr = character.slice(j, j+2);
+                if (cr === "10"){
+                    letter = letter + ".";
+                }else if (cr === "11"){
+                    letter = letter + "-";
+                }
+            }
+            string = string + MORSE_TABLE[letter];
+        }
+    }
+    return string;
 }
 
 module.exports = {
     decode
+}
+
+function decode(expr) {
+    let string = "";
+    for (let i = 0; i < expr.length; i+=10){
+        let letter = "";
+        let character = expr.slice(i, i + 10);
+        if (character === "**********"){
+            string = string + " ";
+        }else{
+            for (let j = 0; j < character.length; j+=2){
+                let cr = character.slice(j, j+2);
+                if (cr === "10"){
+                    letter = letter + ".";
+                }else if (cr === "11"){
+                    letter = letter + "-";
+                }
+            }
+            string = string + MORSE_TABLE[letter];
+        }
+    }
+    return string;
+
 }
